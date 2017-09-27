410830b5ee6f1016713a0998df1963843ce83512
Callie <clarson283@gmail.com>
2017-09-26 22:15:30 -0700

    ugh - cannot get / - what am i doing wrong??

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 var express = require("express");
+var path = require('path');
 var app = express();
 var port = 3000;
 
-app.use("/", (req, res) => {
- res.sendFile(__dirname + "/index.html");
-});
+// app.use("/", (req, res) => {
+//  res.sendFile(__dirname + "/index.html");
+// });
+
+app.use(express.static(path.join(__dirname, 'dest')));
+// app.use(express.static(path.resolve(__dirname, '..', '/dest')));
+
+// app.use((req, res) => {
+//     res.status(400);
+//     res.send('404: This Shit Is Broken');
+// });
 
 app.listen(port, () => {
  console.log("Server listening on port " + port);
